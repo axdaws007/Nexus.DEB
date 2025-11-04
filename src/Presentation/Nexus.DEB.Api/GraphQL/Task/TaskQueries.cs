2507101230c55a58aa094a437b9ee94bb995302c
Alex Dawson <adawson@tlmnexus.com>
2025-11-04 15:55:28 +0000

    Added a query for returning Tasks for the grid

---
+++ src/Presentation/Nexus.DEB.Api/GraphQL/Task/TaskQueries.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate.Authorization;
+using HotChocolate.Resolvers;
+using Nexus.DEB.Application.Common.Interfaces;
+using Nexus.DEB.Application.Common.Models.Filters;
+using Nexus.DEB.Domain.Models;
+
+namespace Nexus.DEB.Api.GraphQL
+{
+    [QueryType]
+    public static class TaskQueries
+    {
+        [Authorize]
+        [UseOffsetPaging]
+        [UseSorting]
+        public static IQueryable<TaskSummary> GetTasksForGrid(
+            TaskSummaryFilters? filters,
+            IDebService debService,
+            IResolverContext resolverContext)
+            => debService.GetTasksForGrid(filters);
+    }
+}

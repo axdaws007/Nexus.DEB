c1fdc2cf872c19fdbd558faed26efeed25a1f83f
Alex Dawson <adawson@tlmnexus.com>
2025-11-05 09:22:09 +0000

    Added TaskType lookup

---
+++ src/Presentation/Nexus.DEB.Api/GraphQL/TaskType/TaskTypeQueries.cs
@@ -0,0 +1,24 @@
+﻿using HotChocolate.Authorization;
+using HotChocolate.Resolvers;
+using Nexus.DEB.Application.Common.Interfaces;
+using Nexus.DEB.Domain.Models;
+
+namespace Nexus.DEB.Api.GraphQL
+{
+    [QueryType]
+    public static class TaskTypeQueries
+    {
+        [Authorize]
+        public static IQueryable<TaskType> GetTaskTypes(
+            IDebService debService,
+            IResolverContext resolverContext)
+            => debService.GetTaskTypes(); 
+        
+        [NodeResolver]
+        public static async Task<TaskType?> GetTaskTypeByIdAsync(
+            short id,
+            TaskTypeByIdDataLoader taskTypeByIdDataLoader,
+            CancellationToken cancellationToken)
+            => await taskTypeByIdDataLoader.LoadAsync(id, cancellationToken);
+    }
+}
---
+++ src/Presentation/Nexus.DEB.Api/GraphQL/TaskType/TaskTypeType.cs
@@ -0,0 +1,15 @@
+﻿using Nexus.DEB.Domain.Models;
+
+namespace Nexus.DEB.Api.GraphQL
+{
+    public class TaskTypeType : ObjectType<TaskType>
+    {
+        protected override void Configure(IObjectTypeDescriptor<TaskType> descriptor)
+        {
+            descriptor.Field(x => x.Id).ID();
+            descriptor.Field(x => x.IsEnabled).Ignore();
+
+            base.Configure(descriptor);
+        }
+    }
+}

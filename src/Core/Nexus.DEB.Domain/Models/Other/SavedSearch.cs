084b3948c9dc5c5518b7487d3b975bdbeb16f398
Mark Seymour <mseymour@tlmnexus.com>
2025-12-03 14:46:16 +0000

    20120 - Saved Search table and Models

---
+++ src/Core/Nexus.DEB.Domain/Models/Other/SavedSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.DEB.Domain.Models
+{
+	public class SavedSearch
+	{
+		public Guid PostId { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Context { get; set; } = string.Empty;
+		public Guid ModuleId { get; set; }
+		public string? Filter { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime? LastModifiedDate { get; set; }
+	}
+}
---
+++ src/Infrastructure/Nexus.DEB.Infrastructure/Configurations/SavedSearchConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Nexus.DEB.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.DEB.Infrastructure.Configurations
+{
+	public class SavedSearchConfiguration : IEntityTypeConfiguration<SavedSearch>
+	{
+		public void Configure(EntityTypeBuilder<SavedSearch> builder)
+		{
+			builder.ToTable("SavedSearch", "common");
+
+			builder.HasKey(x => new { x.PostId, x.Name, x.Context, x.ModuleId });
+
+			builder.Property(x => x.Name).HasMaxLength(350);
+			builder.Property(x => x.Context).HasMaxLength(50);
+			builder.Property(x => x.CreatedDate).IsRequired();
+		}
+	}
+}

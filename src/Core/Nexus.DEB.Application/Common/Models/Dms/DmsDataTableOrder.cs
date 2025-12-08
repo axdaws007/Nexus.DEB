5affe96edb495cad8f51a26842188fc724a46292
Alex Dawson <adawson@tlmnexus.com>
2025-12-08 14:27:26 +0000

    Removed obsolete models

--- src/Core/Nexus.DEB.Application/Common/Models/Dms/DmsDataTableOrder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nexus.DEB.Application.Common.Models.Dms
-{
-    public class DmsDataTableOrder
-    {
-        public string? ColumnName { get; set; }
-        public string? Dir { get; set; } // "asc" or "desc"
-    }
-}
--- src/Core/Nexus.DEB.Application/Common/Models/Dms/DmsDocumentHistoryList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nexus.DEB.Application.Common.Models.Dms
-{
-    public class DmsDocumentHistoryList
-    {
-        public int Draw { get; set; }
-        public int RecordsTotal { get; set; }
-        public List<DmsDocumentHistoryItem> Data { get; set; } = new();
-    }
-}

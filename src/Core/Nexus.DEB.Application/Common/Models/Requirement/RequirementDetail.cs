47716029e61bc341729dad0e39cddea24bb45e26
Mark Seymour <mseymour@tlmnexus.com>
2026-01-07 15:57:42 +0000

    Requirement Summary queries

---
+++ src/Core/Nexus.DEB.Application/Common/Models/Requirement/RequirementDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.DEB.Application.Common.Models
+{
+	public class RequirementDetail : EntityDetailBase
+	{
+		public int RequirementTypeId { get; set; }
+		public string RequirementTypeTitle { get; set; } = string.Empty;
+		public int RequirementCategoryId { get; set; }
+		public string RequirementCategoryTitle { get; set; } = string.Empty;
+	}
+}

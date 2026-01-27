bb2ebec7cdf81726b0ac0f9740272c6f42273cde
Mark Seymour <mseymour@tlmnexus.com>
2026-01-27 10:04:40 +0000

    RequirementIds is just for scope, not striped by StandardVersion

--- src/Core/Nexus.DEB.Application/Common/Models/Scope/StandardVersionRequirements.cs
+++ src/Core/Nexus.DEB.Application/Common/Models/Scope/StandardVersionRequirements.cs
@@ -5,7 +5,6 @@
 		public Guid StandardVersionId { get; set; }
 		public string StandardVersionTitle { get; set; } = string.Empty;
 		public string Status { get; set; } = string.Empty;
-		public List<Guid> RequirementIdsInScope { get; set; } = new List<Guid>();
 		public int TotalRequirementsInScope { get; set; }
 		public int TotalRequirements { get; set; }
 	}
